6ef7f0daf636f27b6ebcf0eb75757c554f6ed621
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-02-15 13:03:41 +0200

    Added handleClickBox Functionality In Board Component

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,25 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.scss";
-import circle_icon from "./assets/circle.png";
-import cross_icon from "./assets/cross.png";
+import circle_icon from "../../assets/circle.png";
+import cross_icon from "../../assets/cross.png";
 
 const Board: React.FC = () => {
+  const [data, setData] = useState<Array<string>>(Array(9).fill(""));
+  const [count, setCount] = useState<number>(0);
+  const [lock, setLock] = useState<boolean>(false);
+
+  const handleClickBox = (num: number) => {
+    if (lock || data[num]) {
+      return;
+    }
+    const newData = [...data];
+    newData[num] = count % 2 === 0 ? "x" : "o";
+    setData(newData);
+    setCount(count + 1);
+  };
+
   return (
     <div className="container-board">
       <div className="row1">
-        <div className="boxes"></div>
-        <div className="boxes"></div>
-        <div className="boxes"></div>
+        {[0, 1, 2].map((index) => (
+          <div
+            key={index}
+            className="boxes"
+            onClick={() => handleClickBox(index)}
+          >
+            {data[index] === "x" && <img src={cross_icon} alt="cross" />}
+            {data[index] === "o" && <img src={circle_icon} alt="circle" />}
+          </div>
+        ))}
       </div>
       <div className="row2">
-        <div className="boxes"></div>
-        <div className="boxes"></div>
-        <div className="boxes"></div>
+        {[3, 4, 5].map((index) => (
+          <div
+            key={index}
+            className="boxes"
+            onClick={() => handleClickBox(index)}
+          >
+            {data[index] === "x" && <img src={cross_icon} alt="cross" />}
+            {data[index] === "o" && <img src={circle_icon} alt="circle" />}
+          </div>
+        ))}
       </div>
       <div className="row3">
-        <div className="boxes"></div>
-        <div className="boxes"></div>
-        <div className="boxes"></div>
+        {[6, 7, 8].map((index) => (
+          <div
+            key={index}
+            className="boxes"
+            onClick={() => handleClickBox(index)}
+          >
+            {data[index] === "x" && <img src={cross_icon} alt="cross" />}
+            {data[index] === "o" && <img src={circle_icon} alt="circle" />}
+          </div>
+        ))}
       </div>
     </div>
   );
