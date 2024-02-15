546ea12283300d0d8a9e93e96f5058f5761b7455
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-02-15 15:30:49 +0200

    Added Square Component

---
+++ src/components/Square/Square.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./styles.scss";
+import circle_icon from "../../assets/circle.png";
+import cross_icon from "../../assets/cross.png";
+
+interface SquareProps {
+  x: number;
+  o: number;
+  disableClick: boolean;
+  winner: string | null;
+  onClick?: () => void;
+}
+
+const Square: React.FC<SquareProps> = ({
+  x,
+  o,
+  winner,
+  onClick,
+  disableClick,
+}) => {
+  return (
+    <div
+      className="square-container"
+      onClick={!disableClick ? onClick : null}
+      style={{ pointerEvents: winner ? "none" : "auto" }}
+    >
+      {x ? (
+        <img src={cross_icon} alt="cross" />
+      ) : o ? (
+        <img src={circle_icon} alt="circle" />
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default Square;
